8ba735895af72d1d5c82878a06403517b35855ba
jpintheinternet <46169761+jpintheinternet@users.noreply.github.com>
2023-03-24 20:15:07 -0700

    Update api.js

--- api.js
+++ api.js
@@ -9,8 +9,8 @@ const apiKey = 'eiwoFSWEcXYP39JpcKVBBUvSUaL2JNZD';
 
 // search for a specific date (keyword) within history of NYT bestsellers lists
 const searchDate = async (keyword) => {
-    // https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=eiwoFSWEcXYP39JpcKVBBUvSUaL2JNZD
-    // https://api.nytimes.com/svc/books/v3/lists/2019-10-10/combined-print-and-e-book-fiction.json?api-key=eiwoFSWEcXYP39JpcKVBBUvSUaL2JNZD
+    // https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=yourkey
+    // https://api.nytimes.com/svc/books/v3/lists/2019-10-10/combined-print-and-e-book-fiction.json?api-key=yourkey
     try {
         // console.log(`input= ${keyword}`);
         const searchURL = `${base}/${keyword}/combined-print-and-e-book-fiction.json?api-key=${apiKey}`;
@@ -24,7 +24,7 @@ const searchDate = async (keyword) => {
 
 // search for reviews of book_selection from previously selected genre
 const searchSelection = async (isbn) => {
-    // https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?isbn=9780735219090&api-key=eiwoFSWEcXYP39JpcKVBBUvSUaL2JNZD
+    // https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?isbn=9780735219090&api-key=yourkey
     try {
         const descriptionURL = `${base}/best-sellers/history.json?isbn=${isbn}&api-key=${apiKey}`;
         const res = await superagent.get(descriptionURL);
